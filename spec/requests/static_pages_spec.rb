1840d9a77589aa822a1decf0f680433b201a4dc8
Nicolás Loaiza <nicoloaiza@yahoo.es>
2014-06-02 11:15:06 -0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,40 +1,31 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  	
+  	subject { page }
 
-  	describe "Home page" do
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			expect(page).to have_title('| Home')
-		end
-
-	    it "should have the content 'Sample App'" do
-	      visit '/static_pages/home'
-	      expect(page).to have_content('Sample App')
-	    end
-  	end
+	describe "Home page" do
+	  	before { visit root_path }
+		it { should have_content('Sample App') }
+		it { should have_title(full_title('')) }
+		it { should_not have_title('| Home') }
+	end
 
 	describe "Help page" do
-		it "should have he title 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-		end
-	    
-		it "should have the content 'Help'" do
-			visit '/static_pages/help'
-			expect(page).to have_content('Help')
-		end
+	  	before { visit help_path }
+		it { should have_content('Help') }
+		it { should have_title(full_title('Help')) }
 	end
 
 	describe "About page" do
-		it "should have the he title 'About'" do
-			visit '/static_pages/about'
-			expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
-		end
-	    
-		it "should have the content 'About us'" do
-			visit '/static_pages/about'
-			expect(page).to have_content('About us')
-		end
+	  	before { visit about_path }
+		it { should have_title(full_title('About'))}
+		it { should have_content('About us') }
+	end	
+
+	describe "Contact page" do
+	  	before { visit contact_path }
+		it { should have_content('Contact') }
+		it { should have_title(full_title('Contact')) }
 	end	
 end
