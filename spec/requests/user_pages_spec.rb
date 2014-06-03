e957b7b9861005c63cd2780ddc9e9d69c537a0c2
Nicolás Loaiza <nicoloaiza@yahoo.es>
2014-06-03 12:36:40 -0500

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,13 +11,13 @@ describe "User pages" do
     it { should have_title(full_title('Sign up')) }
   end
 
-describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
 
-  it { should have_content(user.name) }
-  it { should have_title(user.name) }
-end
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 
   describe "signup page" do
     before { visit signup_path }
