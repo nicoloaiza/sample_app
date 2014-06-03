647affa28e999223b23554a39580dfd846ebfc21
Nicolás Loaiza <nicoloaiza@yahoo.es>
2014-06-03 02:12:08 -0500

    Finish chapter 9 with errors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ describe User do
 
   subject { @user }
 
+
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -16,6 +17,20 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
 
   it { should be_valid }
 
