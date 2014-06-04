1876e08677279f9ba1bc538f36a5338a43ba9e87
Nicolás Loaiza <nicoloaiza@yahoo.es>
2014-06-03 20:37:49 -0500

    finish chapter 10 wiith errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy  
+  before_action :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]  
   
   def show
     @user = User.find(params[:id])
@@ -46,6 +47,20 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end  
 
   private
 
